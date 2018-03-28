c102b53cac20a97b5299b053675f970e7b185d52
guille <ggjnez92@gmail.com>
2018-03-28 11:51:04 -0400

    Create rotate method and it tests

--- test/test.js
+++ test/test.js
@@ -4,13 +4,41 @@ var expect = require('chai').expect;
 var sm = require('../lib/string-mismatch');
 
 describe('string-mismatch tests', function() {
-    it('check how slice work in javascript', function () {
+    it('check how slice work in javascript language', function () {
         var array = [1, 2, 3 ,4];
         expect(array.length).to.equal(4);
 
         var arrayCopy = array.slice();
         expect(arrayCopy.length).to.equal(4);
+    });
 
+    it('check rotate library function', function(){
+        var value = 'asd asd',
+            expected = 'sd asda';
+        expect(sm.rotate(value, 1)).to.equal(expected);
+    });
 
+    it('check rotate library function (undefine or empty value)', function(){
+        var value = 'asd asd',
+            expected = 'asd asd';
+        expect(sm.rotate(value)).to.equal(expected);
+    });
+
+    it('check rotate library function (offset)', function(){
+        var value = 'asd asd',
+            expected = 'sd asda';
+        expect(sm.rotate(value, 8)).to.equal(expected);
+    });
+
+    it('check rotate library function (negative)', function(){
+        var value = 'asd asd',
+            expected = 'dasd as';
+        expect(sm.rotate(value, -1)).to.equal(expected);
+    });
+
+    it('check rotate library function (negative offset)', function(){
+        var value = 'asd asd',
+            expected = 'dasd as';
+        expect(sm.rotate(value, -8)).to.equal(expected);
     });
 });
