f29b7c5ed4f2688147551fff80a5a31e3162b948
wil92 <wil@aleph.engineering>
2019-02-23 22:39:58 -0500

    Add function for copy files to the build directory (LICENSE, README, etc)

--- scripts/build.js
+++ scripts/build.js
@@ -1,14 +1,31 @@
+var fs = require('fs');
+var path = require('path');
 var webpack = require('webpack');
 var webpackConfig = require('../webpack.config');
+
+var projectPath = path.resolve(__dirname, '..');
+
+function copyFile(filePath) {
+    return new Promise(function (resolve, reject) {
+        fs.copyFile(
+            path.resolve(projectPath, filePath),
+            path.resolve(webpackConfig.output.path, filePath),
+            function (err) {
+            return err ? reject(err) : resolve();
+        });
+    });
+}
 
 new Promise(function (resolve, reject) {
     webpack(webpackConfig, function (err, stats) {
         return err ? reject(err) : resolve(stats);
     });
-})
-    .then(function (ignore) {
-        console.log('good', ignore);
-    })
-    .catch(function (err) {
-        console.log('error', err);
-    });
+}).then(function () {
+    return copyFile('package.json')
+}).then(function () {
+    return copyFile('README.md')
+}).then(function () {
+    return copyFile('LICENSE.md');
+}).catch(function (err) {
+    console.log('error', err);
+});
