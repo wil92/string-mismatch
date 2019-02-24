9b9aeb83cb28b8dc620a2c0834e1192b7f28bd8d
wil92 <wil@aleph.engineering>
2019-02-23 23:28:26 -0500

    Create function for remove all files before build

--- scripts/build.js
+++ scripts/build.js
@@ -16,10 +16,51 @@ function copyFile(filePath) {
     });
 }
 
-new Promise(function (resolve, reject) {
-    webpack(webpackConfig, function (err, stats) {
-        return err ? reject(err) : resolve(stats);
+function removeFiles(dirPath, deleteDir) {
+    return new Promise(function (resolve, reject) {
+        if (fs.existsSync(dirPath)) {
+            return fs.readdir(dirPath, function (err, files) {
+                return err ? reject(err) : resolve(files);
+            })
+        }
+        resolve([]);
+    }).then(function (files) {
+        return files.reduce(function (prom, value) {
+            return prom.then(function () {
+                var thePath = path.resolve(dirPath, value);
+                var stats = fs.statSync(thePath);
+                return stats.isDirectory() ? removeFiles(thePath, true) : removeFile(thePath);
+            });
+        }, Promise.resolve());
+    }).then(function () {
+        if (deleteDir) {
+            removeDir(dirPath);
+        }
+    })
+}
+
+function removeDir (dirPath) {
+    return new Promise(function (resolve, reject) {
+        fs.rmdir(dirPath, function (err) {
+            return err ? reject(err) : resolve();
+        })
     });
+}
+
+function removeFile(filePath) {
+    return new Promise(function (resolve, reject) {
+        fs.unlink(filePath, function(err) {
+            return err ? reject(err) : resolve();
+        })
+    });
+}
+
+removeFiles(webpackConfig.output.path).then(function () {
+    return new Promise(function (resolve, reject) {
+        webpack(webpackConfig, function (err, stats) {
+            return err ? reject(err) : resolve(stats);
+        });
+    })
 }).then(function () {
     return copyFile('package.json')
 }).then(function () {
@@ -27,5 +68,5 @@ new Promise(function (resolve, reject) {
 }).then(function () {
     return copyFile('LICENSE.md');
 }).catch(function (err) {
-    console.log('error', err);
+    console.log('ERROR', err);
 });
