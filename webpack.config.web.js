6aabf48ff0b50c87a3bbd43f0bb4f8de1a221da3
wil92 <wil@aleph.engineering>
2019-02-25 19:23:32 -0500

    Add webpack configuration for compatibility with browsers

--- webpack.config.web.js
+++ webpack.config.web.js
@@ -1,16 +1,19 @@
 var path = require('path');
 var libraryName = 'string-mismatch';
+var UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 var config = {
     entry: path.join(__dirname, '/src/string-mismatch.js'),
     output: {
         path: path.resolve(__dirname, 'lib'),
         globalObject: "this",
-        filename: libraryName + '.js',
+        filename: libraryName + '.min.js',
         library: 'sm',
-        libraryTarget: 'umd',
-        umdNamedDefine: true
-    }
+    },
+    devtool: 'source-map',
+    plugins: [
+        new UglifyJSPlugin()
+    ]
 };
 
 module.exports = config;
