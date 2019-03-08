e619fa9b76a2ff88ef2b629d8a425de1521a62f5
wil92 <wil@aleph.engineering>
2019-03-08 11:01:49 -0500

    Create wallaby configuration

---
+++ wallaby.json.js
@@ -0,0 +1,10 @@
+module.exports = function (wallaby) {
+    return {
+        files: [
+            'src/**/*.js'
+        ],
+        tests: [
+            'test/**/*test.js'
+        ]
+    };
+};
