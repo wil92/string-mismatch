286c0f03dcdfcacaab5d6cabda29844517265ea1
wil92 <wil@aleph.engineering>
2019-03-07 21:40:57 -0500

    Fix greedy test broken for the new response format

--- src/algorithms/greedy.test.js
+++ src/algorithms/greedy.test.js
@@ -2,14 +2,14 @@ const chai = require("chai");
 
 const expect = chai.expect;
 
-describe("greedy.js", function() {
+describe("greedy.js", function () {
     let greedy;
 
     beforeEach(function () {
         greedy = require('./greedy')();
     });
 
-    it("check getMatchingSubstring method for find matches", function(){
+    it("check getMatchingSubstring method for find matches", function () {
         var start = "poasdf fdfsaf",
             end = "bgasdfsdfs",
             mValue = 0;
@@ -17,19 +17,25 @@ describe("greedy.js", function() {
         expect(greedy.getMatchingSubstring(start, end, 0, mValue)).to.deep.include({fis: 2, mtc: mValue, sbs: "asdf"});
     });
 
-    it("check diff function with real values 1", function(){
+    it("check diff function with real values 1", function () {
         var start = "This is a test for see how work the library",
-            end = "This is a test for know how work the new library",
-            expected = [ { mtc: "This is a test for ", del: "see", ins: "know", sbs: " how work the " }, { mtc: "", del: "", ins: "new ", sbs: "library" } ];
+            end = "This is a test for know how work the new library";
         // noinspection JSUnresolvedVariable
-        expect(greedy.differences(start, end)).to.deep.equal(expected);
+        expect(greedy.differences(start, end)).to.deep.equal([
+            {type: 'eql', value: 'This is a test for '}, {type: 'del', value: 'see'},
+            {type: 'ins', value: 'know'}, {type: 'eql', value: ' how work the '},
+            {type: 'ins', value: 'new '}, {type: 'eql', value: 'library'}
+        ]);
     });
 
-    it("check diff function with real values without precision parameter", function(){
+    it("check diff function with real values without precision parameter", function () {
         var start = "This is a test for see how work the library",
-            end = "This is a test for know how work the new library",
-            expected = [ { mtc: "This is a test for ", del: "see", ins: "know", sbs: " how work the " }, { mtc: "", del: "", ins: "new ", sbs: "library" } ];
+            end = "This is a test for know how work the new library";
         // noinspection JSUnresolvedVariable
-        expect(greedy.differences(start, end)).to.deep.equal(expected);
+        expect(greedy.differences(start, end)).to.deep.equal([
+            {type: 'eql', value: 'This is a test for '}, {type: 'del', value: 'see'},
+            {type: 'ins', value: 'know'}, {type: 'eql', value: ' how work the '},
+            {type: 'ins', value: 'new '}, {type: 'eql', value: 'library'}
+        ]);
     });
 });
--- src/string-mismatch.test.js
+++ src/string-mismatch.test.js
@@ -32,40 +32,4 @@ describe("string-mismatch tests", function() {
         // noinspection JSUnresolvedVariable
         expect(sm.eraseSpaces(text)).to.equal(expected);
     });
-
-    it("check diffPercent with real data", function(){
-        sm.use(greedy);
-        var start = "Guillermo Gonzalez Jimenez",
-            end = "Guillelmo   Gonzales   Jimenes",
-            expected = { good: true, percent: 0.11538461538461539, diffs: [ { mtc: "Guille", del: "r", ins: "l", sbs: "mo Gonzale" }, { mtc: "", del: "z", ins: "s", sbs: " Jimene" }, { mtc: "", del: "z", ins: "s", sbs: "" } ] };
-        // noinspection JSUnresolvedVariable
-        expect(sm.diffPercent(start, end, 0.6, 5)).to.deep.equal(expected);
-    });
-
-    it("check diffPercent with real data ignore case", function(){
-        sm.use(greedy);
-        var start = "guillermo Gonzalez jimEnEz",
-            end = "Guillelmo   gonzales   Jimenes",
-            expected = {"diffs": [{"del": "r", "ins": "l", "mtc": "Guille", "sbs": "mo gonzale"}, {"del": "z", "ins": "s", "mtc": "", "sbs": " Jimene"}, {"del": "z", "ins": "s", "mtc": "", "sbs": ""}], "good": true, "percent": 0.11538461538461539};
-        // noinspection JSUnresolvedVariable
-        expect(sm.diffPercent(start, end, 0.6, 5, true)).to.deep.equal(expected);
-    });
-
-    it("check diffPercent with real data bug in production", function(){
-        sm.use(greedy);
-        var start = "my name is juan",
-            end = "mi nombre es juan",
-            expected = {"diffs": [{"del": "y", "ins": "i", "mtc": "m", "sbs": " n"}, {"del": "am", "ins": "ombr", "mtc": "", "sbs": "e "}, {"del": "i", "ins": "e", "mtc": "", "sbs": "s juan"}], "good": false, "percent": 0.4};
-        // noinspection JSUnresolvedVariable
-        expect(sm.diffPercent(start, end, 0.9, 2, true)).to.deep.equal(expected);
-    });
-
-    it("should return diffPercent false with real data (bug in production)", function(){
-        sm.use(greedy);
-        var start = "animal",
-            end = "n",
-            expected = {"diffs": [{"del": "a", "ins": "", "mtc": "", "sbs": "n"}, {"del": "imal", "ins": "", "mtc": "", "sbs": ""}], "good": false, "percent": 0.8333333333333334};
-        // noinspection JSUnresolvedVariable
-        expect(sm.diffPercent(start, end, 0.6, 5, true)).to.deep.equal(expected);
-    });
 });
