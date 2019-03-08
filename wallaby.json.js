20c1989aa1d5414695d84da88bf56171ed528934
wil92 <wil@aleph.engineering>
2019-03-08 12:49:24 -0500

    Clean wallaby configuration file

--- wallaby.json.js
+++ wallaby.json.js
@@ -1,20 +1,15 @@
-var wallabify = require('wallabify');
-var wallabyPostprocessor = wallabify({});
-
 module.exports = function (wallaby) {
     return {
         files: [
             'src/**/*.js',
             { pattern: 'src/**/*.test.js', ignore: true }
-            // {pattern: 'src/**/*.js', load: false}
         ],
         tests: [
             'src/**/*.test.js'
-            // {pattern: 'src/**/*.test.js', load: false}
         ],
         testFramework: 'mocha',
         env: {
-            type: 'node',
+            type: 'node'
         }
     };
 };
