001fdc80c29839646359225386d9564542a745c4
wil92 <wil@aleph.engineering>
2019-03-09 21:09:08 -0500

    Update webpack configuration and add config for bundle greedy algorithm

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 var path = require("path");
+
 var UglifyJSPlugin = require("uglifyjs-webpack-plugin");
 var BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 var env = process.env["SM_ENV"] || "production";
 
-var baseCofing = {
+var baseConfig = {
     stats: true,
     optimization: {
         minimizer: [
@@ -22,14 +23,7 @@ var baseCofing = {
             }
         ]
     },
-    devtool: "source-map"
-};
-
-var config = Object.assign({}, baseCofing, {
-    name: "string-mismatch",
-    entry: {
-        "string-mismatch.min": path.join(__dirname, "/src/string-mismatch.js")
-    },
+    devtool: "source-map",
     output: {
         path: path.resolve(__dirname, "lib"),
         globalObject: "this",
@@ -42,28 +36,35 @@ var config = Object.assign({}, baseCofing, {
     plugins: env === "development" ? [new BundleAnalyzerPlugin({
         analyzerPort: 0
     })] : []
+};
+
+var configsModules = [
+    {
+        name: "string-mismatch",
+        entry: {"string-mismatch.min": path.join(__dirname, "/src/string-mismatch.js")},
+        output: {library: "sm"}
+    },
+    {
+        name: "levenshtein",
+        entry: {"levenshtein.min": path.join(__dirname, "/src/algorithms/levenshtein.js")},
+        output: {library: "levenshtein"}
+    },
+    {
+        name: "greedy",
+        entry: {"greedy.min": path.join(__dirname, "/src/algorithms/greedy.js")},
+        output: {library: "greedy"}
+    }
+];
+
+
+var configs = configsModules.map(function (config) {
+    return Object.assign({}, baseConfig, {
+        name: config.name,
+        entry: config.entry,
+        output: Object.assign({}, baseConfig.output, config.output)
+    });
 });
 
-var configLevenshtein = Object.assign({}, baseCofing, {
-    name: "levenshtein",
-    entry: {
-        "levenshtein.min": path.join(__dirname, "/src/algorithms/levenshtein.js")
-    },
-    output: {
-        path: path.resolve(__dirname, "lib"),
-        globalObject: "this",
-        filename: "[name].js",
-        library: "levenshtein",
-        libraryTarget: "umd",
-        libraryExport: "default",
-        umdNamedDefine: true
-    },
-    plugins: env === "development" ? [new BundleAnalyzerPlugin({
-        analyzerPort: 0
-    })] : []
-});
-
-var configs = [config, configLevenshtein];
 var notMinify = configs.map(function (config) {
     var entry = {}, key = Object.keys(config.entry)[0];
     entry[key.replace(".min", "")] = config.entry[key];
@@ -73,6 +74,5 @@ var notMinify = configs.map(function (config) {
         plugins: []
     });
 });
-configs = notMinify.concat(configs);
 
-module.exports = configs;
+module.exports = notMinify.concat(configs);
