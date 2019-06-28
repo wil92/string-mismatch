5fb3144e1ef2b47e7d7a3e049f6eafce789a4c21
wil92 <wil@aleph.engineering>
2019-06-27 21:59:59 -0400

    Create dice-coefficient unit tests (#9)

---
+++ src/algorithms/dice-coefficient.test.js
@@ -0,0 +1,25 @@
+import chai from "chai";
+import spy from "chai-spies";
+
+import {DiceCoefficient} from "./dice-coefficient";
+
+chai.use(spy);
+const expect = chai.expect;
+
+describe("dice-coefficient.js", function () {
+    let dice;
+
+    beforeEach(function () {
+        dice = new DiceCoefficient();
+    });
+
+    it("should call the diceCoefficientAlgorithm method", function () {
+        chai.spy.on(dice, "diceCoefficientAlgorithm", () => 0.5);
+        expect(dice.distance("start", "end")).equal(0.5);
+        expect(dice.diceCoefficientAlgorithm).to.have.been.called();
+    });
+
+    it("should return the string distance", function () {
+        expect(dice.diceCoefficientAlgorithm("sten", "end")).equal(0.4);
+    });
+});
